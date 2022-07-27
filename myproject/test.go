42643244a62cf60f8876d064cd9999bb924e6cb3
Michael Chen <pinyuchen1225@gmail.com>
2022-07-27 14:50:46 +0800

    22/7/27

--- myproject/test.go
+++ myproject/test.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"strconv"
 )
 
-//接收一個interface{}型別的函數
-func doubler(v interface{}) (string, error) {
-	if i, ok := v.(int); ok { //嘗試轉成int
-		return fmt.Sprint(i * 2), nil
-	}
-	if s, ok := v.(string); ok { //嘗試轉成string
-		return s + s, nil
-	}
-	//型別不符前面的檢查 , 傳回錯誤
-	return "", errors.New("傳入了未支援的值")
-}
-
 func main() {
-	res, _ := doubler(5) 
-	fmt.Println("5   :", res) //印出 5   : 10
-	res, _ = doubler("yum")  
-	fmt.Println("yum :", res) //印出  yum : yumyum
-	_, err := doubler(true)
-	fmt.Println("true:", err) //印出 true: 傳入了為支援的值
+	var str interface{} = 42
+	if s, ok := str.(string); ok { //做型別斷言 , 並檢查是否轉換成功
+		fmt.Println(strconv.Atoi(s))
+	} else {
+		fmt.Println("Type assertion failed")
+	}
 }
-
